e92058c6f20ec21adab97065983f8a98cdf81954
Jheng-Jhong Wu <goodwater.wu@gmail.com>
2016-08-26 17:07:14 +0800

    Add an example for relocatable queue
    1. Add relocatable member to struct rlq
    2. Add enum relocate
    3. Add functions:
    	RLQ_INIT_RELOCATE()
    	rlq_init_relocate()
    	rlq_relocate()
    	jjwu_rlq_enqueue()
    	jjwu_rlq_dequeue()
    4. Modify functions:
    	RLQ_INIT()
    	rlq_init()
    	rlq_enqueue()
    	rlq_dequeue()

--- include/jjwu_rlq.h
+++ include/jjwu_rlq.h
@@ -25,7 +25,16 @@
 /* The following definition should not change manually. */
 
 #define RLQ_INIT(NAME, SIZE)\
-	RLQ NAME = { .head = 0, .tail = 0, .size = SIZE, .available = SIZE, .data = calloc(SIZE, 1) }
+	RLQ NAME = { .head = 0, .tail = 0, .size = SIZE, .available = SIZE, .relocatable = RLQ_NOT_RELOCATABLE, .data = calloc(SIZE, 1) }
+
+#define RLQ_INIT_RELOCATE(NAME, SIZE)\
+	RLQ NAME = { .head = 0, .tail = 0, .size = SIZE, .available = SIZE, .relocatable = RLQ_RELOCATABLE, .data = calloc(SIZE, 1) }
+
+typedef enum relocate
+{
+	RLQ_NOT_RELOCATABLE = 0,
+	RLQ_RELOCATABLE = 1,
+} RELOCATE;
 
 typedef struct rlq
 {
@@ -34,6 +43,7 @@ typedef struct rlq
 	unsigned int size;
 	unsigned int available;
 	unsigned int occupied;
+	RELOCATE relocatable;
 	void *data;
 } RLQ, *PRLQ;
 
@@ -44,10 +54,31 @@ inline void rlq_init(PRLQ prlq, unsigned int size, void *buffer)
 	prlq->size = size;
 	prlq->available = size;
 	prlq->occupied = 0;
+	prlq->relocatable = RLQ_NOT_RELOCATABLE;
 	prlq->data = buffer;
 }
 
-inline unsigned int rlq_enqueue(PRLQ prlq, const void *src, unsigned int size)
+inline void rlq_init_relocate(PRLQ prlq, unsigned int size, void *buffer)
+{
+	prlq->head = 0;
+	prlq->tail = 0;
+	prlq->size = size;
+	prlq->available = size;
+	prlq->occupied = 0;
+	prlq->relocatable = RLQ_RELOCATABLE;
+	prlq->data = buffer;
+}
+
+inline void rlq_clear(PRLQ prlq)
+{
+	bzero(prlq->data, prlq->size);
+	prlq->head = 0;
+	prlq->tail = 0;
+	prlq->available = prlq->size;
+	prlq->occupied = 0;
+}
+
+inline unsigned int jjwu_rlq_enqueue(PRLQ prlq, const void *src, unsigned int size)
 {
 	unsigned int copy_size = 0;
 
@@ -67,7 +98,40 @@ inline unsigned int rlq_enqueue(PRLQ prlq, const void *src, unsigned int size)
 	return copy_size;
 }
 
-inline unsigned int rlq_dequeue(PRLQ prlq, void *dest, unsigned int size)
+inline void rlq_relocate(PRLQ prlq)
+{
+	if (prlq->occupied != 0)
+	{
+		if (prlq->tail != 0)
+		{
+			unsigned int size = prlq->occupied;
+			void *tmp = calloc(size, 1);
+
+			if (tmp != NULL)
+			{
+				memcpy(tmp, prlq->data+prlq->tail, size);
+				rlq_clear(prlq);
+				jjwu_rlq_enqueue(prlq, tmp, size);
+			}
+
+			free(tmp);
+		}
+	}
+	else
+	{
+		rlq_clear(prlq);
+	}
+}
+
+inline unsigned int rlq_enqueue(PRLQ prlq, const void *src, unsigned int size)
+{
+	if (prlq->relocatable)
+		rlq_relocate(prlq);
+
+	return jjwu_rlq_enqueue(prlq, src, size);
+}
+
+inline unsigned int jjwu_rlq_dequeue(PRLQ prlq, void *dest, unsigned int size)
 {
 	unsigned int copy_size = 0;
 
@@ -86,13 +150,14 @@ inline unsigned int rlq_dequeue(PRLQ prlq, void *dest, unsigned int size)
 	return copy_size;
 }
 
-inline void rlq_clear(PRLQ prlq)
+inline unsigned int rlq_dequeue(PRLQ prlq, void *dest, unsigned int size)
 {
-	bzero(prlq->data, prlq->size);
-	prlq->head = 0;
-	prlq->tail = 0;
-	prlq->available = prlq->size;
-	prlq->occupied = 0;
+	unsigned int dequeue_size = jjwu_rlq_dequeue(prlq, dest, size);
+
+	if (prlq->relocatable)
+		rlq_relocate(prlq);
+
+	return dequeue_size;
 }
 
 inline int rlq_empty(PRLQ prlq)
@@ -153,6 +218,7 @@ inline void rlq_statistics(PRLQ prlq)
 	printf("size: %u\n", prlq->size);
 	printf("available: %u\n", prlq->available);
 	printf("occupied: %u\n", prlq->occupied);
+	printf("relocatable: %u\n", prlq->relocatable);
 }
 
 #endif
